d09a2f470569e0b0a5ea937b6d93eea4dee8752b
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-30 13:03:00 -0400

    functioning add div + img for one kitten

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  const button1 = document.querySelector('.summon-cats');
+
+  button1.addEventListener('click', function() {
+    axios.get('http://bitkittens.herokuapp.com/cats.json').then(function(response){
+      let catsList = response.data.cats;
+      console.log(catsList);
+      let catdiv1 = document.getElementById(
+        "cat1");
+      let catImg = document.createElement('img');
+      catImg.src = catsList[0].photo;
+      catdiv1.appendChild(catImg);
+    });
+  });
 });
