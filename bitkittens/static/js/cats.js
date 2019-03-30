cdff85cf37309ab9a0b6d0c650896bff3d9d6480
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-30 13:28:40 -0400

    made code more performant

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -6,11 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     axios.get('http://bitkittens.herokuapp.com/cats.json').then(function(response){
       let catsList = response.data.cats;
       console.log(catsList);
-      let catdiv1 = document.getElementById(
-        "cat1");
-      let catdiv2 = document.getElementById("cat2");
-      let catdiv3 = document.getElementById("cat3");
-      let divArray = [catdiv1, catdiv2, catdiv3];
+      let divArray = document.querySelectorAll(".cat-box");
       for (var i = 0; i < divArray.length; i++) {
         divArray[i].innerHTML = null;
         let catImg = document.createElement('img');
